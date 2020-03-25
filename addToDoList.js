43462cdc7940d5f1878ec65e5d36ee60de8b9093
Andrii Solohub <andrei.sologub12@gmail.com>
2020-03-25 18:23:04 +0200

    a little changes with get/post request

--- addToDoList.js
+++ addToDoList.js
@@ -1,48 +1,49 @@
+
 let requestURL = 'https://solo-todo-app.herokuapp.com/tasks';
-let xhr = new XMLHttpRequest();
-xhr.open('get', requestURL);
-xhr.onload = function(){
-    let taskResponse = JSON.parse(this.responseText);
-    console.log(taskResponse);
-    let checkboxList = document.getElementById("checkbox-list");
-    // let checkboxStatus = document.getElementById("myCheck");
-    
-    for (task of taskResponse){
-        let checked = '';
-         if (task.done){
-             checked = 'checked'
-         }
-        let listItem = `<li class='list-items'><input type='checkbox' id='myCheck' ${checked}>${task.name}<button class='delete-btn button is-dark is-small' id='delete-item'>Delete</button></li>`;
-        checkboxList.innerHTML += listItem;
-       
+
+function getReq(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', requestURL);
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.send()
+    xhr.onload = function(){
+        let resp = JSON.parse(xhr.responseText);
+        console.log(resp);
+        resp.forEach(function(task){
+            let someItem = document.getElementById('checkbox-list');
+            let newLi = `<li class='list-items' id='li-item${task.id}'>
+                            <input type='checkbox' id='myCheck'>${task.name}
+                            <button class='delete-btn button is-dark is-small' id='delete-item'>Delete</button>
+                        </li>`;
+            someItem.innerHTML += newLi;
+        });
+        
+        let delBtn = document.querySelectorAll('ul button');
+        delBtn.forEach(elem => {
+            elem.addEventListener('click', function(){
+                let liAndButton = elem.closest(`li`);
+                    console.log(liAndButton ,' deleted');
+                liAndButton.remove();
+            })
+        })       
     }
-    
 }
-xhr.send();
+getReq();
 
 
 
-
+function postReq(x){
+    let xhrPost = new XMLHttpRequest();
+    let noLong = JSON.stringify(x);
+        xhrPost.open('POST', requestURL);
+        xhrPost.setRequestHeader('content-type', 'application/json');
+        xhrPost.send(noLong);
+}
 
 
 let btnPlusTodo = document.getElementById("btnPlusTodo");
 btnPlusTodo.addEventListener("click", function(){
-    
-    let checkboxList = document.getElementById("checkbox-list"); 
-    let newText = document.getElementById("new-text").value;
-    let listItem = `<li class='list-items'><input type='checkbox'>${newText}<button class='delete-btn button is-dark is-small' id='delete-item'>Delete</button></li>`;
-    checkboxList.innerHTML += listItem;
-    console.log(newText)
-
-    let xhrPost = new XMLHttpRequest();
-    let jsonText = JSON.stringify({name: newText});
-    xhrPost.open('post', requestURL);
-    xhrPost.setRequestHeader('content-type', 'application/json');
-    xhrPost.onreadystatechange = function(){
-        if(xhrPost.readyState == XMLHttpRequest.DONE && xhrPost.status == 200) console.log("ok");
-    }
-    xhrPost.send(jsonText);
+    let inpSectionText = document.querySelector('#new-text').value;
+    let textObj = {name: inpSectionText}
+    postReq(textObj);
 })
-
-
-
