8dfb71c813cce8dc13c748c4807de021a3cac5bf
Andrii Solohub <andrei.sologub12@gmail.com>
2020-03-25 22:40:28 +0200

    refactoring

--- addToDoList.js
+++ addToDoList.js
@@ -1,49 +1,75 @@
 
 let requestURL = 'https://solo-todo-app.herokuapp.com/tasks';
 
-function getReq(){
+function loadAndDisplayTasks() {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', requestURL);
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.send()
-    xhr.onload = function(){
-        let resp = JSON.parse(xhr.responseText);
-        console.log(resp);
-        resp.forEach(function(task){
-            let someItem = document.getElementById('checkbox-list');
-            let newLi = `<li class='list-items' id='li-item${task.id}'>
-                            <input type='checkbox' id='myCheck'>${task.name}
-                            <button class='delete-btn button is-dark is-small' id='delete-item'>Delete</button>
-                        </li>`;
-            someItem.innerHTML += newLi;
-        });
-        
-        let delBtn = document.querySelectorAll('ul button');
-        delBtn.forEach(elem => {
-            elem.addEventListener('click', function(){
-                let liAndButton = elem.closest(`li`);
-                    console.log(liAndButton ,' deleted');
-                liAndButton.remove();
-            })
-        })       
+
+    xhr.onload = function () {
+        let allTasks = JSON.parse(xhr.responseText);
+        allTasks.forEach(displayTask);    
     }
+    xhr.send();
 }
-getReq();
+loadAndDisplayTasks();
 
+function removeTask(taskId) {
+    let deleteRequest = new XMLHttpRequest();
+    deleteRequest.onload = function () {
+        let taskListItem = document.getElementById(`li-item${taskId}`);
+        taskListItem.remove();
+    }
+    deleteRequest.open('DELETE', requestURL + `/${taskId}`);
+    deleteRequest.send();
+}
 
-
-function postReq(x){
-    let xhrPost = new XMLHttpRequest();
-    let noLong = JSON.stringify(x);
-        xhrPost.open('POST', requestURL);
-        xhrPost.setRequestHeader('content-type', 'application/json');
-        xhrPost.send(noLong);
+function addTask(taskName) {
+    let taskObject = { name: taskName };
+    let addTaskRequest = new XMLHttpRequest();
+    let jsonTask = JSON.stringify(taskObject);
+    addTaskRequest.open('POST', requestURL);
+    addTaskRequest.setRequestHeader('content-type', 'application/json');
+    addTaskRequest.onload = function () {
+        let taskResponse = JSON.parse(addTaskRequest.responseText);
+        displayTask(taskResponse);
+        console.log(taskResponse);
+    }
+    addTaskRequest.send(jsonTask);
 }
 
 
 let btnPlusTodo = document.getElementById("btnPlusTodo");
-btnPlusTodo.addEventListener("click", function(){
-    let inpSectionText = document.querySelector('#new-text').value;
-    let textObj = {name: inpSectionText}
-    postReq(textObj);
+btnPlusTodo.addEventListener("click", function () {
+    let input = document.getElementById('new-text');
+    addTask(input.value);
+    input.value = '';
 })
+
+
+function displayTask(task) {
+    let someItem = document.getElementById('checkbox-list');
+    let newLi = `<li class='list-items'  id='li-item${task.id}'>
+                    <input type='checkbox' ${task.done ? 'checked' : ''} id='myCheck' onclick='changeState(${task.id})'><label>${task.name}</label>
+                    <button class='delete-btn button is-dark is-small' id='delete-item' onclick='removeTask(${task.id})'>Delete</button>
+                </li>`;
+    someItem.innerHTML += newLi;
+};
+
+function changeState(taskId){
+    let isDone = document.querySelector(`#li-item${taskId} input`).checked;
+    
+    let updateRequst = new XMLHttpRequest();
+    updateRequst.open('POST' , requestURL+ `/${taskId}`);
+    updateRequst.setRequestHeader('content-type', 'application/json');
+    updateRequst.send(JSON.stringify({done: isDone}));
+
+    // ${task.done ? 'task-done':''}
+    //let item = document.getElementById(`li-item${taskId}`)
+    
+    //if (isDone){
+    //     item.classList.add('task-done');
+    // }else{
+    //     item.classList.remove('task-done');
+    // }
+}
+
